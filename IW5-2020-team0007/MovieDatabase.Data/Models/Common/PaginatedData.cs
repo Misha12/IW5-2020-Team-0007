0d833c9e45b0289eb238aa39aad5812cb0c79db1
MichalHalabica <m.halabica@gmail.com>
2020-04-21 16:58:07 +0200

    Stránkování.

---
+++ IW5-2020-team0007/MovieDatabase.Data/Models/Common/PaginatedData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MovieDatabase.Data.Models.Common
+{
+    public class PaginatedData<TModel>
+    {
+        public List<TModel> Data { get; set; }
+        
+        public int PageNumber { get; set; }
+        public long TotalItemsCount { get; set; }
+        
+        public bool CanNext { get; set; }
+        public bool CanPrev { get; set; }
+    }
+}
