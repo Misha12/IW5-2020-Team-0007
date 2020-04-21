4962837e135294061d83acf3c05b7415fa62a7d2
MichalHalabica <m.halabica@gmail.com>
2020-04-21 17:46:44 +0200

    Drobné technické úpravy.

---
+++ IW5-2020-team0007/MovieDatabase.Data/Models/Common/PaginatedRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieDatabase.Data.Models.Common
+{
+    public class PaginatedRequest
+    {
+        [Range(0, 200, ErrorMessage = "Limit je mimo povolený rozsah")]
+        public int Limit { get; set; }
+        
+        [Range(0, double.MaxValue, ErrorMessage = "Číslo stránky je mimo povolený rozsah.")]
+        public int Page { get; set; }
+    }
+}
