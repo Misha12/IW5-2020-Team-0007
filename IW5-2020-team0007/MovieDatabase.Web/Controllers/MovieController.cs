f97483b788b9ba77ec4ae4858fc2ed5f405b04a3
Viktor Konupčík <xkonup01@vutbr.cz>
2020-05-17 12:22:32 +0200

    Moviecontroller init

---
+++ IW5-2020-team0007/MovieDatabase.Web/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDatabase.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
