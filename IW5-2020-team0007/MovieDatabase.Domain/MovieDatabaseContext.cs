9d2733ac09f610ee92fbab05bf1f55fb39886547
MichalHalabica <m.halabica@gmail.com>
2020-05-17 02:15:42 +0200

    Odstranění starých souborů.

--- IW5-2020-team0007/MovieDatabase.Domain/MovieDatabaseContext.cs
+++ IW5-2020-team0007/MovieDatabase.Domain/MovieDatabaseContext.cs
@@ -13,21 +13,9 @@ namespace MovieDatabase.Domain
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Movie>().HasMany(o => o.Names).WithOne(o => o.Movie);
-            modelBuilder.Entity<Movie>().HasMany(o => o.Rates).WithOne(o => o.Movie);
-            modelBuilder.Entity<Movie>().HasOne(o => o.Genre).WithMany(o => o.Movies);
-
-            modelBuilder.Entity<Movie>().HasMany(o => o.Persons).WithOne(o => o.Movie);
-            modelBuilder.Entity<Person>().HasMany(o => o.InMovies).WithOne(o => o.Person);
-
-            modelBuilder.Entity<MoviePerson>()
-                .HasKey(o => new { o.MovieID, o.PersonID, o.Type });
         }
 
-        public virtual DbSet<Genre> Genres { get; set; }
-        public virtual DbSet<Movie> Movies { get; set; }
-        public virtual DbSet<Person> Persons { get; set; }
         public virtual DbSet<Rate> Rates { get; set; }
+        public virtual DbSet<Movie> Movies { get; set; }
     }
 }
