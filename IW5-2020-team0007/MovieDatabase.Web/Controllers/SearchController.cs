68b6f512baeab4b97398306146903978dbe0c937
xkoude26 <xkoude26@vutbr.cz>
2020-05-18 23:21:33 +0200

    Search test

---
+++ IW5-2020-team0007/MovieDatabase.Web/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieDatabase.BL.Web.Facades;
+using MovieDatabase.Web.ViewModels;
+
+namespace MovieDatabase.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private protected SearchFacade _searchFacade;
+        public SearchController(SearchFacade facade)
+        {
+            _searchFacade = facade;
+        }
+
+
+        public async Task<IActionResult> Search(String search, int? page)
+        {
+            var searchViewModel = new SearchViewModel
+            {
+                SearchResultBase = await SearchF(search,page)
+            };
+            return View(searchViewModel);
+        }
+
+        [HttpGet]
+        public async Task<PaginatedDataOfSearchResultBase> SearchF(String name, int? page)
+        {
+            return await _searchFacade.SearchAsync(HttpContext.User.FindFirst(ClaimTypes.Hash).Value, name, 10, page);
+        }
+
+    }
+}
