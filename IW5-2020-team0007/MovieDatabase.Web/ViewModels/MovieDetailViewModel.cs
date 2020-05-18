f22fa2dbb27d2aed6ac94dcd53702d1fbaf21111
MichalHalabica <m.halabica@gmail.com>
2020-05-18 23:13:19 +0200

    Dokončení detailu filmu. Drobné UI změny.

--- IW5-2020-team0007/MovieDatabase.Web/ViewModels/MovieDetailViewModel.cs
+++ IW5-2020-team0007/MovieDatabase.Web/ViewModels/MovieDetailViewModel.cs
@@ -10,6 +10,8 @@ namespace MovieDatabase.Web.ViewModels
         public Movie DetailMovieModel { get; set; }
         public PaginatedDataOfRating ListRatingModel { get; set; }
         public List<Genre> Genres { get; set; }
+        public List<PersonFilterItem> Persons { get; set; }
+        public bool? SaveSuccess { get; set; }
 
         public int AvgRatingScore => Convert.ToInt32(ListRatingModel.Data.Average(o => o.Score));
     }
--- IW5-2020-team0007/MovieDatabase.Web/ViewModels/MovieUpdateViewModel.cs
+++ IW5-2020-team0007/MovieDatabase.Web/ViewModels/MovieUpdateViewModel.cs
@@ -8,6 +8,7 @@ namespace MovieDatabase.Web.ViewModels
     public class MovieUpdateViewModel
     {
         public long ID { get; set; }
+        public TimeSpan Length { get; set; }
         public EditMovieRequest MovieRequest { get; set; }
     }
 }
