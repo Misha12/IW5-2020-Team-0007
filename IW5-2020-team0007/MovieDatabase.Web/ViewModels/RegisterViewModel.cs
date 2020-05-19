d6f2d8358e4cb8baaddfe6057e1d6e948a48a8ee
MichalHalabica <m.halabica@gmail.com>
2020-05-19 02:02:17 +0200

    Registrace

--- IW5-2020-team0007/MovieDatabase.Web/ViewModels/RegisterViewModel.cs
+++ IW5-2020-team0007/MovieDatabase.Web/ViewModels/RegisterViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace MovieDatabase.Web.ViewModels
+﻿using System.Collections.Generic;
+
+namespace MovieDatabase.Web.ViewModels
 {
     public class RegisterViewModel
     {
         public RegisterRequest UserModel { get; set; }
+        public List<string> Messages { get; set; }
+        public bool? Success { get; set; }
     }
 }
